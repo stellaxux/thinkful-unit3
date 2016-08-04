682fb229136eda65141f43a0f810248feaf9b171
Stella Xu <xinxu0824@gmail.com>
2016-08-04 16:14:24 -0400

    repeat query

--- citibike.py
+++ citibike.py
@@ -8,7 +8,11 @@ Created on Wed Aug  3 22:13:59 2016
 import requests
 import sqlite3 as lite
 import time
+import collections
+import pandas as pd
 from dateutil.parser import parse
+import matplotlib.pyplot as plt
+import datetime
 
 con = lite.connect('citi_bike.db')
 cur = con.cursor()
@@ -34,3 +38,36 @@ for i in range(60):
 
 #close the database connection when done
 con.close()
+
+# collect and store the change for each station every minute
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+
+df = pd.read_sql_query("SELECT * FROM available_bikes ORDER BY execution_time",con,index_col='execution_time')
+hour_change = collections.defaultdict(int)
+
+for col in df.columns:
+    station_id = col[1:]
+    station_change = df[col].diff()
+    hour_change[int(station_id)] = abs(station_change[1:]).sum()
+
+# Find the key with the greatest value
+def maxstation(d):
+    return max(d, key = lambda k: d[k])
+    
+# assign the max key to max_station
+max_station = maxstation(hour_change)
+
+#query sqlite for reference information
+cur.execute("SELECT id, stationname, latitude, longitude FROM citibike_reference WHERE id = ?", (max_station,))
+data = cur.fetchone()
+
+print("The most active station is station id %s at %s latitude: %s longitude: %s " % data)
+print("With %d bicycles coming and going in the hour between %s and %s" % (
+    hour_change[max_station],
+    datetime.datetime.fromtimestamp(int(df.index[0])).strftime('%Y-%m-%d %H:%M:%S'),
+    datetime.datetime.fromtimestamp(int(df.index[-1])).strftime('%Y-%m-%d %H:%M:%S'),
+))
+
+plt.bar(hour_change.keys(), hour_change.values())
+plt.show()
