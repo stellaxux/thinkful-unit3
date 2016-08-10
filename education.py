ba58f9017b750f0e43ac07394058c882b9aadaa1
Stella Xu <xinxu0824@gmail.com>
2016-08-10 12:24:23 -0400

    Unit3Lesson3

--- education.py
+++ education.py
@@ -10,6 +10,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import csv
 import sqlite3 as lite
+import numpy as np
 
 url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm"
 r = requests.get(url)
@@ -59,6 +60,7 @@ df['Total'].median()
 con = lite.connect('gdp.db')
 cur = con.cursor()
 with con:
+    cur.execute("DROP TABLE IF EXISTS gdp")
     cur.execute('CREATE TABLE gdp (country_name TEXT, _1999 NUMERIC, _2000 NUMERIC, _2001 NUMERIC,\
                  _2002 NUMERIC, _2003 NUMERIC, _2004 NUMERIC, _2005 NUMERIC, _2006 NUMERIC, _2007 NUMERIC, _2008 NUMERIC, _2009 NUMERIC, _2010 NUMERIC)')
 
@@ -75,3 +77,25 @@ with open('world_bank_data/GDP.csv', 'rU') as inputFile:
             cur.execute('INSERT INTO gdp (country_name, _1999, _2000, _2001,\
             _2002, _2003, _2004, _2005, _2006, _2007, _2008, _2009, _2010) VALUES ("'+line[0]+'", "'+'","'.join(line[43:55]) + '");')
 
+table_gdp = pd.read_sql_query('SELECT * FROM gdp', con)
+
+# find common countries
+country_gdp = table_gdp['country_name'].tolist()
+country_un = df['Country or area'].tolist()
+country_common = list(set(country_gdp) & set(country_un))
+
+gdp = []
+total = []
+men = []
+women = []
+for country in country_common:
+    df1 = df[df['Country or area']==country]
+    df2 = table_gdp[table_gdp['country_name']==country]
+    if (df2['_'+ df1['Year']].iloc[0].iloc[0] != ''):
+        total.append(df1['Total'].iloc[0])
+        men.append(df1['Men'].iloc[0])
+        women.append(df1['Women'].iloc[0])
+        gdp.append(np.log(df2['_'+ df1['Year']].iloc[0].iloc[0]))
+gdp_schoollife = pd.DataFrame({'Total': total, 'Men': men, 'Women': women, 'GDP': gdp})
+
+print gdp_schoollife.corr()
