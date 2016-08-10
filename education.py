738f66801080990fd3b008c1c123b9239ced252b
Stella Xu <xinxu0824@gmail.com>
2016-08-10 01:25:49 -0400

    Unit2Lesson3

---
+++ education.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  9 11:50:34 2016
+
+@author: Xin
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import csv
+import sqlite3 as lite
+
+url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm"
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content)
+
+#for row in soup('table'):
+#    print(row)
+
+# locate the table
+tb = soup('table')[6]('table')[1]
+
+# find all the tags
+#for tag in tb.find_all(True):
+#    print(tag.name)
+
+# return all the strings in the table
+content = []
+for string in tb.tr.stripped_strings:
+    if string in ['a','b','c','d','e','f','g','h']:
+        continue
+    content.append(string)
+content = content[9:]
+header = ['Country or area', 'Year', 'Total', 'Men', 'Women']
+
+# reshape and put them into pandas dataframe
+content_reshape = [content[i:i+5] for i in range(0, len(content), 5)]
+df = pd.DataFrame(content_reshape, columns = header)
+df[['Total', 'Men', 'Women']] = df[['Total', 'Men', 'Women']].astype(int) 
+
+# plot distribution
+df['Men'].hist()
+df['Women'].hist()
+df['Total'].hist()
+
+# median and mean age for each gender
+df['Men'].mean()
+df['Women'].mean()
+df['Total'].mean()
+
+df['Men'].median()
+df['Women'].median()
+df['Total'].median()
+
+
+# create gdp database
+con = lite.connect('gdp.db')
+cur = con.cursor()
+with con:
+    cur.execute('CREATE TABLE gdp (country_name TEXT, _1999 NUMERIC, _2000 NUMERIC, _2001 NUMERIC,\
+                 _2002 NUMERIC, _2003 NUMERIC, _2004 NUMERIC, _2005 NUMERIC, _2006 NUMERIC, _2007 NUMERIC, _2008 NUMERIC, _2009 NUMERIC, _2010 NUMERIC)')
+
+## Compare GDP to education attainment
+with open('world_bank_data/GDP.csv', 'rU') as inputFile:
+    next(inputFile) # skip the first two lines
+    next(inputFile)
+    next(inputFile)
+    next(inputFile)
+    header = next(inputFile)
+    inputReader = csv.reader(inputFile)
+    for line in inputReader:
+        with con:
+            cur.execute('INSERT INTO gdp (country_name, _1999, _2000, _2001,\
+            _2002, _2003, _2004, _2005, _2006, _2007, _2008, _2009, _2010) VALUES ("'+line[0]+'", "'+'","'.join(line[43:55]) + '");')
+
